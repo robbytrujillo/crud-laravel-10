876cbcc06950b5174cc21b9e89f4acbf6b408fc9
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-05-20 13:04:45 +0700

    CRUD Laravel 10 learning

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,6 +13,10 @@ use Illuminate\Http\RedirectResponse;
 
 use Illuminate\Http\Request;
 
+// import Facade "Storage" (20052023)
+use Illuminate\Support\Facades\Storage;
+
+
 class PostController extends Controller
 {
     public function index(): View {
@@ -51,5 +55,67 @@ class PostController extends Controller
         // redirect to index (20052023)
         return redirect()->route('posts.index')->with(['success' => 'Data berhasil disimpan!']);
     }
+
+    // membuat fuction show (20052023)
+    public function show(string $id): View {
+        
+        // get post by ID (20052023)
+        $post = Post::findOrFail($id);
+
+        // render view with post (20052023)
+        return view('posts.show', compact('post'));
+}
+
+// membuat fuction ubah dan hapus (20052023)
+public function edit(string $id): View {
+    // get post by ID (20052023)
+    $post = Post::findOrFail($id);
+
+    // render view with post
+    return view ('posts.edit', compact('post'));
+}
+
+// membuat fuction update (20052023)
+public function update(Request $request, $id): RedirectResponse {
+    
+    $this->validate($request, [
+        'image' => 'image|mimes:jpeg,jpg,png|max:2048',
+        'title' => 'required|min:5',
+        'content' => 'required|min:10'
+    ]);
+
+    // get post by ID (20052023)
+    $post = Post::findOrFail($id);
+
+    // cek if image is upload (20052023)
+    if ($request->hasFile('image')) {
+
+        // upload new image (20052023)
+        $image = $request->file('image');
+        $image->storeAs('public/posts', $image->hashName());
+
+        // delete old image (20052023)
+        Storage::delete('public/posts/' . $post->image);
+
+        // update post with new image
+        $post->update([
+            'image' => $image->hashName(),
+            'title' => $request->title,
+            'content' => $request->content
+        ]);
+
+    } else {
+
+        // update post without image (20052023)
+        $post->update([
+            'title' => $request->title,
+            'content' => $request->content
+        ]);
+    }
+
+    // redirect to index (20052023)
+    return redirect()->route('posts.index')->with(['success' => 'Data Berhasil diubah']);
+
 }
     
+}
